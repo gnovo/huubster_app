76cb283b1f71eedb6144f4217b5e0ff64cc9262f
Goncalo Novo <goncalo.novo@protonmail.com>
2017-10-02 23:51:30 +0100

    added pages views, updated pages_controller, added pages to routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,30 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:about, :careers, :contact, :home, :partners, :press, :privacy, :site_map, :terms]
+
+  def about
+  end
+
+  def careers
+  end
+
+  def contact
+  end
 
   def home
+  end
+
+  def partners
+  end
+
+  def press
+  end
+
+  def privacy
+  end
+
+  def site_map
+  end
+
+  def terms
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  scope '(:locale)', locale: /en|pt/ do
+    root to: 'pages#home'
+    get 'about' => 'pages#about'
+    get 'careers' => 'pages#careers'
+    get 'contact' => 'pages#contact'
+    get 'faq' => 'pages#faq'
+    get 'partners' => 'pages#partners'
+    get 'press' => 'pages#press'
+    get 'pricing' => 'pages#pricing'
+    get 'privacy' => 'pages#privacy'
+    get 'site_map' => 'pages#site_map'
+    get 'terms' => 'pages#terms'
+  end
 end
